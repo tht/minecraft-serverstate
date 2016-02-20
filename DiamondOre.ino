c237ec4c6b6d6eb62127d34d4a92702856573cdc
Thomas Lohmueller <thomas@lohmueller.ch>
2016-02-20 22:55:04 +0100

    Local compile-time configuration moved to EEPRIM with cloud-function for updating

--- DiamondOre.ino
+++ DiamondOre.ino
@@ -1,27 +1,106 @@
 #include "serverstate.h"
 
 #define UPDATE_INTERVAL 30 * 1000 // all 30s
+#define EEPROM_BASE 0x00
+#define EEPROM_HEAD 0x62
 
-ServerState server("home.lohmueller.ch", 25565);
+ServerState server;
+
+struct {
+  char server_name[32];
+  uint16_t server_port;
+} config = { "", 25565 };
+bool config_changed = false;
+
+bool read_config() {
+  if (EEPROM.read(EEPROM_BASE) != EEPROM_HEAD) {
+    Serial.println("No valid configuration in EEPROM.");
+    Serial.println("Use exposed functions to set configuration.");
+    return false;
+  }
+
+  Serial.println("EEPROM has a valid header. Reading configuration.");
+  EEPROM.get(EEPROM_BASE+1, config);
+  config_changed = true;
+  return true;
+}
+
+void store_config() {
+  EEPROM.put(EEPROM_BASE+1, config);
+  EEPROM.write(EEPROM_BASE, EEPROM_HEAD);
+}
+
+int cloud_set_server(String _server) {
+  Serial.print("Received new server from Cloud: "); Serial.println(_server);
+
+  int name_length = _server.indexOf(':');
+  int tmp_port = 0;
+
+  if (name_length != -1) {
+    String port = _server.substring(name_length+1, _server.length());
+    Serial.print("Trying to parse port number: "); Serial.println(port);
+    tmp_port = port.toInt();
+    if (!tmp_port) {
+      Serial.println("Parsing port number failed.");
+      return -1;
+    }
+    Serial.print("New port: "); Serial.println(tmp_port, DEC);
+  } else {
+    Serial.println("No port number supplied, using 25565 (default).");
+    tmp_port = 25565;
+    name_length = _server.length();
+  }
+
+  // Check length of servername
+  int max_size = sizeof(config.server_name)-1;
+  if (_server.length() > max_size) {
+    Serial.print("Servername to long. Max is: "); Serial.println(max_size);
+    return -1;
+  }
+
+  // Looks valid - store new port and server name
+  config.server_port = tmp_port;
+  for (int i=0; i<name_length; i++) {
+    config.server_name[i] = _server.charAt(i);
+  }
+  config.server_name[name_length] = 0;
+  print_config();
+  store_config();
+  server.set_server(config.server_name, config.server_port);
+  config_changed = true;
+}
+
+void print_config() {
+  Serial.print("Configured for: "); Serial.print(config.server_name);
+  Serial.print(" ("); Serial.print(config.server_port); Serial.println(')');
+}
 
 void setup() {
   Serial.begin(57600);
   Serial.println("Starting up...");
 
-  /*
-  // Register Variables to Particle Cloud
-  Particle.variable("serverName", server_name);
-  Particle.variable("serverPort", server_port);
-  */
+  // Register configuration function to Particle Cloud
+  Particle.function("setServer", cloud_set_server);
+
+  // Try to read configuration from EEPROM
+  if (read_config()) {
+    Serial.println("Got configuration from EEPROM.");
+    print_config();
+    server.set_server(config.server_name, config.server_port);
+  }
 }
 
 void loop() {
-  static unsigned long lastChecked = 2*UPDATE_INTERVAL;
+  static unsigned long lastChecked = 0;
   static unsigned int numRepeats = 0;
 
-  if (millis() - lastChecked > (unsigned long) UPDATE_INTERVAL) {
+  if (config_changed || millis() - lastChecked > (unsigned long) UPDATE_INTERVAL) {
+    config_changed = false;
     lastChecked = millis();
-    Serial.print("Starting check #"); Serial.println(++numRepeats);
-    server.query_server();
+
+    if (server.configured()) {
+      Serial.print("Starting check #"); Serial.println(++numRepeats);
+      server.query_server();
+    }
   }
 }
--- serverstate.h
+++ serverstate.h
@@ -15,20 +15,26 @@ public:
     STAT_ONLINE
   };
 
+  ServerState() { }
   ServerState(char* _name, uint16_t _port) { set_server(_name, _port); }
   void set_server(char* _name, uint16_t _port);
   void query_server();
+  bool configured() {return server_name != 0;}
+
+  int get_state() {return server_state;}
+  int get_players_online() {return players_onl;}
+  int get_max_players() {return players_max;}
 
 private:
+  // Configuration
   char *server_name;
   uint16_t server_port;
 
+  // State
   uint16_t players_max = 0;
   uint16_t players_onl = 0;
   char *description;
-
   enum server_state_t server_state;
-  uint8_t server_players;
 
   int read_length(TCPClient *client);
   int read_int(char *buffer, TCPClient *client, int *ri);
